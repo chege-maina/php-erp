f5cc2e0af12bf22ded6983b2616e5672162cc704
Timothy Karani <kartimothy@gmail.com>
2021-02-01 18:38:58 +0300

    styled code

--- base-page.php
+++ base-page.php
@@ -27,11 +27,15 @@
       <?php
       include './includes/base_page/nav.php';
       ?>
+
       <div class="content">
         <?php
         include './navbar-shared.php';
         ?>
+
+        <!-- =========================================================== -->
         <!-- body begins here -->
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
         <div class="card">
           <div class="card-header bg-light">
             <h5 class="mb-0">Change My Title</h5>
@@ -48,41 +52,23 @@
             </div>
             <!-- Content ends here -->
           </div>
+          <!-- Additional cards can be added here -->
         </div>
-        <footer>
-          <div class="row g-0 justify-content-between fs--1 mt-4 mb-3">
-            <div class="col-12 col-sm-auto text-center">
-              <p class="mb-0 text-600">Thank you for creating with Falcon <span class="d-none d-sm-inline-block">| </span><br class="d-sm-none" /> 2021 &copy; <a href="https://themewagon.com">Themewagon</a></p>
-            </div>
-            <div class="col-12 col-sm-auto text-center">
-              <p class="mb-0 text-600">v3.0.0-alpha11</p>
-            </div>
-          </div>
-        </footer>
-      </div>
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <!-- body ends here -->
+        <!-- =========================================================== -->
 
-      <!-- Modals begin here --->
-      <?php
-      include './includes/base_page/modal-settings.php';
-      ?>
-      <?php
-      include './includes/base_page/modal-authentication.php';
-      ?>
-      <!-- End of Modals -->
 
-    </div>
-  </main>
-  <script src="../vendors/popper/popper.min.js"></script>
-  <script src="../vendors/bootstrap/bootstrap.min.js"></script>
-  <script src="../vendors/anchorjs/anchor.min.js"></script>
-  <script src="../vendors/is/is.min.js"></script>
-  <script src="../vendors/fontawesome/all.min.js"></script>
-  <script src="../vendors/lodash/lodash.min.js"></script>
-  <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
-  <script src="../vendors/list.js/list.min.js"></script>
-  <script src="../assets/js/theme.js"></script>
-
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:100,200,300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
+        
+        <!-- =========================================================== -->
+        <!-- Footer Begin -->
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <?php
+        include './includes/base_page/footer.php';
+        ?>
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <!-- Footer End -->
+        <!-- =========================================================== -->
 </body>
 
 </html>
