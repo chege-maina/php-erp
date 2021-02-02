fbd205b59d90f0f53ef61c8f0f3c126198293c12
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-02 12:47:22 +0300

    update

--- products/add-product-ui.php
+++ products/add-product-ui.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en-US" dir="ltr">
 <?php
+session_start();
+  // If the user is not logged in redirect to the login page...
+  if (!isset($_SESSION['loggedin'])) {
+    header('Location: index.html');
+    exit();
+  }
+  include_once 'includes/dbconnect.php';
 include '../includes/base_page/head.php';
 ?>
 
