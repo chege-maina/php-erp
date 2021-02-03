d4fc20d0db38324b895a761116e91297534aeadb
RuthKerubo <ruthkeggy@gmail.com>
2021-02-03 14:51:53 +0300

    made constraints in add_product.php

--- products/add_product.php
+++ products/add_product.php
@@ -14,6 +14,8 @@ if ( !!$_FILES['product_image']['tmp_name'] ) // is the file uploaded yet?
         if ( move_uploaded_file( $_FILES['product_image']['tmp_name'], $todir . basename($_FILES['product_image']['name'] ) ) )
         {
             // the file has been moved correctly
+            $result = mysqli_query("SELECT * FROM tbl_product WHERE product_code=$product_code") or die(mysql_error());
+            if (mysqli_num_rows($result) > 0){
             if (isset($_POST["submit"])){
 	
 	$sql ="INSERT INTO `tbl_product`( `product_code`, `product_name`, `product_unit`, `product_category`, `min_level`,`max_level`,`reorder`, `product_image`,`dsp_price` )  values(NULL,'$_POST[product_name]', '$_POST[product_unit]', '$_POST[product_category]', '$_POST[min_level]', '$_POST[max_level]', '$_POST[reorder]', '/assets/img/item-images/".$_FILES['product_image']['name']."', '$_POST[dsp_price]') ";
@@ -27,20 +29,21 @@ if ( !!$_FILES['product_image']['tmp_name'] ) // is the file uploaded yet?
 	}
 
 }
+            }
         }
         else
     {
-        // error this file could not be uploaded
+    echo "error this file could not be uploaded":
     }
     }
     else
     {
-        // error this file ext is not allowed
+      echo "  error this file ext is not allowed";
     }
 }
 else
     {
-        // error file not uploaded
+    echo "error file not uploaded";
     }
 
 
