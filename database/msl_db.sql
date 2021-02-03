837473b43f187c17cb03ec1eea164cee460d3750
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-03 13:47:04 +0300

    database

---
+++ database/msl_db.sql
@@ -0,0 +1,265 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 03, 2021 at 11:46 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `msl_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_branch`
+--
+
+CREATE TABLE `tbl_branch` (
+  `branch_id` int(11) NOT NULL,
+  `branch_name` varchar(100) NOT NULL,
+  `branch_location` varchar(100) NOT NULL,
+  `contact_person` varchar(100) NOT NULL,
+  `telephone_no` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category`
+--
+
+CREATE TABLE `tbl_category` (
+  `category_code` int(11) NOT NULL,
+  `category_name` varchar(254) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_category`
+--
+
+INSERT INTO `tbl_category` (`category_code`, `category_name`) VALUES
+(2, 'CEMENT'),
+(1, 'METAL BARS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `product_code` bigint(254) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_unit` varchar(254) NOT NULL,
+  `product_category` varchar(254) NOT NULL,
+  `min_level` varchar(50) NOT NULL,
+  `max_level` varchar(50) NOT NULL,
+  `reorder` varchar(50) NOT NULL,
+  `product_image` varchar(254) NOT NULL,
+  `dsp_price` int(254) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+INSERT INTO `tbl_product` (`product_code`, `product_name`, `product_unit`, `product_category`, `min_level`, `max_level`, `reorder`, `product_image`, `dsp_price`) VALUES
+(1, 'TANK', 'Piece', 'tanks', '12', '13', '13', 'weretrtet.png', 0),
+(2, 'TARAJI', 'PIECES', 'PENCIL', '9', '17', '33', 'OIP.jpg', 3000),
+(4, 'image upload', 'asd,jbhkj', 'asdhgjkh', '344', '33333', '33333', '/assets/img/item-images/php.png', 0),
+(6, 'CHANK', 'asd,jbhkj', 'TANK', '667', '667', '76', '/assets/img/item-images/php.png', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_requisition`
+--
+
+CREATE TABLE `tbl_requisition` (
+  `requisition_No` bigint(20) NOT NULL,
+  `date` date NOT NULL,
+  `time` varchar(50) NOT NULL,
+  `user` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_requisition_items`
+--
+
+CREATE TABLE `tbl_requisition_items` (
+  `id` int(11) NOT NULL,
+  `requisition_No` bigint(100) NOT NULL,
+  `prroduct_code` varchar(100) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_unit` varchar(10) NOT NULL,
+  `product_quantity` varchar(200) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tax`
+--
+
+CREATE TABLE `tbl_tax` (
+  `col_tax` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_unit`
+--
+
+CREATE TABLE `tbl_unit` (
+  `unit_code` int(11) NOT NULL,
+  `product_unit` varchar(254) NOT NULL,
+  `unit_description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_unit`
+--
+
+INSERT INTO `tbl_unit` (`unit_code`, `product_unit`, `unit_description`) VALUES
+(1, 'KGS', 'KILOGRAMS'),
+(2, 'BGS', 'BAGS'),
+(3, 'PCS', 'PIECES'),
+(4, 'LTRS', 'LITRES');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `email` varchar(50) NOT NULL,
+  `password` varchar(254) NOT NULL,
+  `designation` varchar(50) NOT NULL,
+  `branch` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `status` varchar(3) NOT NULL,
+  `level` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`email`, `password`, `designation`, `branch`, `first_name`, `last_name`, `status`, `level`) VALUES
+('dir@maisha.com', '$2y$10$Jisk2Nl0cHrTa0id8f2kIeQ9My1mruHswrJwj3J1tMenC538wbPCa', 'director', 'mm2', 'Kesav', 'Kesav', 'ON', 'ON'),
+('procurement.maishasteel@gmail.com', 'procurement123', 'procurement', 'main', 'James', 'Kevin', 'ON', 'OFF'),
+('storemanager.maishasteel@gmail.com', 'storemanager123', 'store manager', 'mm1', 'Jael', 'Joel', 'OFF', 'ON');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_branch`
+--
+ALTER TABLE `tbl_branch`
+  ADD PRIMARY KEY (`branch_id`);
+
+--
+-- Indexes for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`category_code`),
+  ADD UNIQUE KEY `category_name` (`category_name`);
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`product_code`),
+  ADD UNIQUE KEY `product_name` (`product_name`);
+
+--
+-- Indexes for table `tbl_requisition`
+--
+ALTER TABLE `tbl_requisition`
+  ADD PRIMARY KEY (`requisition_No`);
+
+--
+-- Indexes for table `tbl_requisition_items`
+--
+ALTER TABLE `tbl_requisition_items`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `requisition_No` (`requisition_No`);
+
+--
+-- Indexes for table `tbl_unit`
+--
+ALTER TABLE `tbl_unit`
+  ADD PRIMARY KEY (`unit_code`),
+  ADD UNIQUE KEY `product_unit` (`product_unit`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_branch`
+--
+ALTER TABLE `tbl_branch`
+  MODIFY `branch_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  MODIFY `category_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `product_code` bigint(254) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_requisition`
+--
+ALTER TABLE `tbl_requisition`
+  MODIFY `requisition_No` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_requisition_items`
+--
+ALTER TABLE `tbl_requisition_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_unit`
+--
+ALTER TABLE `tbl_unit`
+  MODIFY `unit_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
