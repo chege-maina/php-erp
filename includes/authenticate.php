1388ecf9421987808044b6b9473a300df2d0ab7d
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-04 14:03:34 +0300

    login sessions

--- includes/authenticate.php
+++ includes/authenticate.php
@@ -21,7 +21,7 @@ if ($stmt = $con->prepare('SELECT email, password, designation, branch, first_na
 	// Store the result so we can check if the account exists in the database.
 	$stmt->store_result();
 if ($stmt->num_rows > 0) {
-	$stmt->bind_result($email, $password, $desigantion, $branch, $first_name, $last_name);
+	$stmt->bind_result($email, $password, $designation, $branch, $first_name, $last_name);
 	$stmt->fetch();
 	// Account exists, now we verify the password.
 	// Note: remember to use password_hash in your registration file to store the hashed passwords.
@@ -32,7 +32,7 @@ if ($stmt->num_rows > 0) {
 		$_SESSION['loggedin'] = TRUE;
 		$_SESSION['branch'] = $branch;
 		$_SESSION['name'] = $first_name." ".$last_name;
-		$_SESSION['id'] = $_POST['email'];
+		$_SESSION['designation'] = $designation;
 
 		echo 'Dashboard';
 
