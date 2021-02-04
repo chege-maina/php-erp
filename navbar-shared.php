9e0bec4d177ce540dddc3957347ba9c6e7a12192
Timothy Karani <kartimothy@gmail.com>
2021-02-04 12:06:55 +0300

    Added branch info to header

--- navbar-shared.php
+++ navbar-shared.php
@@ -5,113 +5,12 @@
     <div class="d-flex align-items-center"><img class="mr-2" src="../assets/img/illustrations/falcon.png" alt="" width="40" /><span class="font-sans-serif">falcon</span>
     </div>
   </a>
-  <ul class="navbar-nav align-items-center d-none d-lg-block">
-    <li class="nav-item">
-      <div class="search-box" data-list='{"valueNames":["title"]}'>
-        <form class="position-relative" data-toggle="search" data-display="static">
-          <input class="form-control search-input fuzzy-search" type="search" placeholder="Search..." aria-label="Search" />
-          <span class="fas fa-search search-box-icon"></span>
-
-        </form>
-        <button class="btn-close position-absolute right-0 top-50 translate-middle shadow-none p-1 mr-1 fs--2" type="button" data-dismiss="search"></button>
-        <div class="dropdown-menu border font-base left-0 mt-2 py-0 overflow-hidden w-100">
-          <div class="scrollbar list py-3" style="max-height: 24rem;">
-            <h6 class="dropdown-header fw-medium text-uppercase px-card fs--2 pt-0 pb-2">Recently Browsed</h6><a class="dropdown-item fs--1 px-card py-1 hover-primary" href="../pages/events.html">
-              <div class="d-flex align-items-center">
-                <span class="fas fa-circle mr-2 text-300 fs--2"></span>
-
-                <div class="fw-normal title">Pages <span class="fas fa-chevron-right mx-1 text-500 fs--2" data-fa-transform="shrink-2"></span> Events</div>
-              </div>
-            </a>
-            <a class="dropdown-item fs--1 px-card py-1 hover-primary" href="../e-commerce/customers.html">
-              <div class="d-flex align-items-center">
-                <span class="fas fa-circle mr-2 text-300 fs--2"></span>
-
-                <div class="fw-normal title">E-commerce <span class="fas fa-chevron-right mx-1 text-500 fs--2" data-fa-transform="shrink-2"></span> Customers</div>
-              </div>
-            </a>
-
-            <hr class="bg-200" />
-            <h6 class="dropdown-header fw-medium text-uppercase px-card fs--2 pt-0 pb-2">Suggested Filter</h6><a class="dropdown-item px-card py-1 fs-0" href="../e-commerce/customers.html">
-              <div class="d-flex align-items-center"><span class="badge fw-medium text-decoration-none mr-2 badge-soft-warning">customers:</span>
-                <div class="flex-1 fs--1 title">All customers list</div>
-              </div>
-            </a>
-            <a class="dropdown-item px-card py-1 fs-0" href="../pages/events.html">
-              <div class="d-flex align-items-center"><span class="badge fw-medium text-decoration-none mr-2 badge-soft-success">events:</span>
-                <div class="flex-1 fs--1 title">Latest events in current month</div>
-              </div>
-            </a>
-            <a class="dropdown-item px-card py-1 fs-0" href="../e-commerce/product-grid.html">
-              <div class="d-flex align-items-center"><span class="badge fw-medium text-decoration-none mr-2 badge-soft-info">products:</span>
-                <div class="flex-1 fs--1 title">Most popular products</div>
-              </div>
-            </a>
-
-            <hr class="bg-200" />
-            <h6 class="dropdown-header fw-medium text-uppercase px-card fs--2 pt-0 pb-2">Files</h6><a class="dropdown-item px-card py-2" href="#!">
-              <div class="d-flex align-items-center">
-                <div class="file-thumbnail mr-2"><img class="border h-100 w-100 fit-cover rounded-3" src="../assets/img/products/3-thumb.png" alt="" /></div>
-                <div class="flex-1">
-                  <h6 class="mb-0 title">iPhone</h6>
-                  <p class="fs--2 mb-0"><span class="fw-semi-bold">Antony</span><span class="fw-medium text-600 ml-2">27 Sep at 10:30 AM</span></p>
-                </div>
-              </div>
-            </a>
-            <a class="dropdown-item px-card py-2" href="#!">
-              <div class="d-flex align-items-center">
-                <div class="file-thumbnail mr-2"><img class="img-fluid" src="../assets/img/icons/zip.png" alt="" /></div>
-                <div class="flex-1">
-                  <h6 class="mb-0 title">Falcon v1.8.2</h6>
-                  <p class="fs--2 mb-0"><span class="fw-semi-bold">John</span><span class="fw-medium text-600 ml-2">30 Sep at 12:30 PM</span></p>
-                </div>
-              </div>
-            </a>
-
-            <hr class="bg-200" />
-            <h6 class="dropdown-header fw-medium text-uppercase px-card fs--2 pt-0 pb-2">Members</h6><a class="dropdown-item px-card py-2" href="../pages/profile.html">
-              <div class="d-flex align-items-center">
-                <div class="avatar avatar-l status-online mr-2">
-                  <img class="rounded-circle" src="../assets/img/team/1.jpg" alt="" />
-
-                </div>
-                <div class="flex-1">
-                  <h6 class="mb-0 title">Anna Karinina</h6>
-                  <p class="fs--2 mb-0">Technext Limited</p>
-                </div>
-              </div>
-            </a>
-            <a class="dropdown-item px-card py-2" href="../pages/profile.html">
-              <div class="d-flex align-items-center">
-                <div class="avatar avatar-l mr-2">
-                  <img class="rounded-circle" src="../assets/img/team/2.jpg" alt="" />
-
-                </div>
-                <div class="flex-1">
-                  <h6 class="mb-0 title">Antony Hopkins</h6>
-                  <p class="fs--2 mb-0">Brain Trust</p>
-                </div>
-              </div>
-            </a>
-            <a class="dropdown-item px-card py-2" href="../pages/profile.html">
-              <div class="d-flex align-items-center">
-                <div class="avatar avatar-l mr-2">
-                  <img class="rounded-circle" src="../assets/img/team/3.jpg" alt="" />
-
-                </div>
-                <div class="flex-1">
-                  <h6 class="mb-0 title">Emma Watson</h6>
-                  <p class="fs--2 mb-0">Google</p>
-                </div>
-              </div>
-            </a>
-
-          </div>
-        </div>
-      </div>
-    </li>
-  </ul>
   <ul class="navbar-nav navbar-nav-icons ml-auto flex-row align-items-center">
+    <ul class="navbar-nav align-items-center d-none d-lg-block">
+      <li class="nav-item">
+        <h6 id="branch" class="mt-3 mr-2">Branch Name</h6>
+      </li>
+    </ul>
     <li class="nav-item"><a class="nav-link settings-popover" href="#settings-modal" data-toggle="modal"><span class="ripple"></span><span class="fa-spin position-absolute all-0 d-flex flex-center"><span class="icon-spin position-absolute all-0 d-flex flex-center">
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M19.7369 12.3941L19.1989 12.1065C18.4459 11.7041 18.0843 10.8487 18.0843 9.99495C18.0843 9.14118 18.4459 8.28582 19.1989 7.88336L19.7369 7.59581C19.9474 7.47484 20.0316 7.23291 19.9474 7.03131C19.4842 5.57973 18.6843 4.28943 17.6738 3.20075C17.5053 3.03946 17.2527 2.99914 17.0422 3.12011L16.393 3.46714C15.6883 3.84379 14.8377 3.74529 14.1476 3.3427C14.0988 3.31422 14.0496 3.28621 14.0002 3.25868C13.2568 2.84453 12.7055 2.10629 12.7055 1.25525V0.70081C12.7055 0.499202 12.5371 0.297594 12.2845 0.257272C10.7266 -0.105622 9.16879 -0.0653007 7.69516 0.257272C7.44254 0.297594 7.31623 0.499202 7.31623 0.70081V1.23474C7.31623 2.09575 6.74999 2.8362 5.99824 3.25599C5.95774 3.27861 5.91747 3.30159 5.87744 3.32493C5.15643 3.74527 4.26453 3.85902 3.53534 3.45302L2.93743 3.12011C2.72691 2.99914 2.47429 3.03946 2.30587 3.20075C1.29538 4.28943 0.495411 5.57973 0.0322686 7.03131C-0.051939 7.23291 0.0322686 7.47484 0.242788 7.59581L0.784376 7.8853C1.54166 8.29007 1.92694 9.13627 1.92694 9.99495C1.92694 10.8536 1.54166 11.6998 0.784375 12.1046L0.242788 12.3941C0.0322686 12.515 -0.051939 12.757 0.0322686 12.9586C0.495411 14.4102 1.29538 15.7005 2.30587 16.7891C2.47429 16.9504 2.72691 16.9907 2.93743 16.8698L3.58669 16.5227C4.29133 16.1461 5.14131 16.2457 5.8331 16.6455C5.88713 16.6767 5.94159 16.7074 5.99648 16.7375C6.75162 17.1511 7.31623 17.8941 7.31623 18.7552V19.2891C7.31623 19.4425 7.41373 19.5959 7.55309 19.696C7.64066 19.7589 7.74815 19.7843 7.85406 19.8046C9.35884 20.0925 10.8609 20.0456 12.2845 19.7729C12.5371 19.6923 12.7055 19.4907 12.7055 19.2891V18.7346C12.7055 17.8836 13.2568 17.1454 14.0002 16.7312C14.0496 16.7037 14.0988 16.6757 14.1476 16.6472C14.8377 16.2446 15.6883 16.1461 16.393 16.5227L17.0422 16.8698C17.2527 16.9907 17.5053 16.9504 17.6738 16.7891C18.7264 15.7005 19.4842 14.4102 19.9895 12.9586C20.0316 12.757 19.9474 12.515 19.7369 12.3941ZM10.0109 13.2005C8.1162 13.2005 6.64257 11.7893 6.64257 9.97478C6.64257 8.20063 8.1162 6.74905 10.0109 6.74905C11.8634 6.74905 13.3792 8.20063 13.3792 9.97478C13.3792 11.7893 11.8634 13.2005 10.0109 13.2005Z" fill="#2A7BE4"></path>
@@ -185,22 +84,16 @@
 
     </li>
     <li class="nav-item dropdown"><a class="nav-link pr-0" id="navbarDropdownUser" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <div class="avatar avatar-xl">
-          <img class="rounded-circle" src="../assets/img/team/3-thumb.png" alt="" />
+        <div>
+          <span class="fa fa-user-circle fa-2x mt-0"></span>
 
         </div>
       </a>
       <div class="dropdown-menu dropdown-menu-right py-0" aria-labelledby="navbarDropdownUser">
         <div class="bg-white dark__bg-1000 rounded-2 py-2">
-          <a class="dropdown-item fw-bold text-warning" href="#!"><span class="fas fa-crown mr-1"></span><span>Go Pro</span></a>
-
+          <div id="name" class="dropdown-item">Your Name</div>
+          <div id="designation" class="dropdown-item">Your Designation</div>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#!">Set status</a>
-          <a class="dropdown-item" href="../pages/profile.html">Profile &amp; account</a>
-          <a class="dropdown-item" href="#!">Feedback</a>
-
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="../pages/settings.html">Settings</a>
           <a class="dropdown-item" href="../authentication/card/logout.html">Logout</a>
         </div>
       </div>
