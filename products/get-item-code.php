53ea2300f985fe7e3500acf6937e0e1f1d9b0df6
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-04 11:34:34 +0300

    requisition code

--- products/get-item-code.php
+++ products/get-item-code.php
@@ -2,12 +2,12 @@
 <?php
 include_once '../includes/dbconnect.php';
 
-$count=0;
+$count=1;
 $select = "SELECT product_code FROM tbl_product ORDER BY product_code DESC LIMIT  1";
 $result = mysqli_query($conn,$select);
 if($row = mysqli_fetch_assoc($result)){
 	$id = $row['product_code'];
-	$count = $id + $count+1;
+	$count = $id + $count;
 }
 echo $count;
 
