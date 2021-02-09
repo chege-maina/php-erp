ef781c50fdb0d774d8b4800dbfd266b065fea3fa
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-09 10:19:08 +0300

    manage requisition

---
+++ includes/requisition_manage_items.php
@@ -0,0 +1,34 @@
+<?php
+include_once '../includes/dbconnect.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $req_no = $_POST["req_no"];
+
+  $query = "SELECT * FROM tbl_requisition_items WHERE requisition_No ='$req_no'";
+        	
+	$result = mysqli_query($conn, $query);
+    $response = array();
+	
+	while($row = mysqli_fetch_assoc($result)){
+        array_push($response, 
+				   array(
+                   'code'=>$row['product_code'],
+                   'name'=>$row['product_name'],
+                   'unit'=>$row['product_unit'],
+                   'qty'=>$row['product_quantity'],
+                   'balance'=>$row['balance'])
+                   );
+                }
+
+                echo json_encode($response);
+
+mysqli_close($conn);
+            }
+            else{
+                $message = "Fields have no data...";
+                echo json_encode($message);
+            }
+
+?>
+    
