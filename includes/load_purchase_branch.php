bfffc834a2fdfda00f3cdb469a1b9872c96b4de7
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-09 13:40:38 +0300

    purchases fetch

---
+++ includes/load_purchase_branch.php
@@ -0,0 +1,22 @@
+<?php
+include_once '../includes/dbconnect.php';
+session_start();
+
+$query = "SELECT count(branch), branch FROM tbl_requisition WHERE status= 'approved' GROUP BY branch ASC";
+	
+	$result = mysqli_query($conn, $query);
+	$response = array();
+	
+	while($row = mysqli_fetch_assoc($result)){
+		array_push($response, 
+				   array(
+				   'branch'=>$row['branch'],
+				   'count'=>$row['count(branch)'])
+				   );
+	}
+	echo json_encode($response);
+
+mysqli_close($conn);
+          
+
+?>
