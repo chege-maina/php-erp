e9c1f734dbd972eb20e7cd85d87de83d37959c6c
Timothy Karani <kartimothy@gmail.com>
2021-02-09 09:27:37 +0300

    Filtering table

--- includes/filter_requisitions.php
+++ includes/filter_requisitions.php
@@ -6,32 +6,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $start_date = $_POST["date1"];
   $end_date = $_POST["date2"];
   $status = $_POST["status"];
-  $branch =$_SESSION['branch'];
+  $branch = $_SESSION['branch'];
+
 
   $query = "SELECT * FROM tbl_requisition WHERE status='$status' and branch='$branch' and date>='$start_date' and date<= '$end_date'";
-        	
-	$result = mysqli_query($conn, $query);
-    $response = array();
-	
-	while($row = mysqli_fetch_assoc($result)){
-        array_push($response, 
-				   array(
-                   'req_no'=>$row['requisition_No'],
-                   'date'=>$row['date'],
-                   'branch'=>$row['branch'],
-                   'user'=>$row['user'],
-                   'status'=>$row['status'])
-                   );
-                }
 
-                echo json_encode($response);
+  $result = mysqli_query($conn, $query);
+  $response = array();
 
-mysqli_close($conn);
-            }
-            else{
-                $message = "Fields have no data...";
-                echo json_encode($message);
-            }
+  while ($row = mysqli_fetch_assoc($result)) {
+    array_push(
+      $response,
+      array(
+        'req_no' => $row['requisition_No'],
+        'date' => $row['date'],
+        'branch' => $row['branch'],
+        'user' => $row['user'],
+        'status' => $row['status']
+      )
+    );
+  }
 
-?>
-    
+  echo json_encode($response);
+
+  mysqli_close($conn);
+} else {
+  $message = "Fields have no data...";
+  echo json_encode($message);
+}
+
