6dff522f2537f9dd0412daaf03400eeef89a56c6
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-10 09:40:22 +0300

    sidenav

--- includes/base_page/sidenav-shared.php
+++ includes/base_page/sidenav-shared.php
@@ -49,7 +49,7 @@ function genSideBar()
   </div>
 </a>
 <ul class="nav collapse" id="email" data-parent="#navbarVerticalCollapse">
-  <li class="nav-item"><a class="nav-link" href="../purchase_orders/create_purchase_order.php">Create Purchase Order</a>
+  <li class="nav-item"><a class="nav-link" href="../purchase_orders/select_po.php">Create Purchase Order</a>
   </li>
   <li class="nav-item"><a class="nav-link" href="../email/email-detail.html">Purchase Order</a>
   </li>
