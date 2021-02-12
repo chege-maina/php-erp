5df3d10faa126e54a59ea922366ae3a06c66c8db
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-12 13:16:04 +0300

    iii

--- includes/receive_goods_manage_items.php
+++ includes/receive_goods_manage_items.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stats = 'pending';
     $stats2 = 'approved';
 
-    $query = "SELECT * FROM tbl_purchaseorder_items WHERE po_number ='$req_no''";
+    $query = "SELECT * FROM tbl_purchaseorder_items WHERE po_number ='$req_no'";
 
     $result = mysqli_query($conn, $query);
     $response = array();
