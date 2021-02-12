75102a3a68e0f79c53e909f587ca59dd02cb7420
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-12 15:23:35 +0300

    hhh

---
+++ includes/add_items_supplier.php
@@ -0,0 +1,77 @@
+<?php
+include_once '../includes/dbconnect.php';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $supplier_name = $_POST["supplier_name"];
+    $branch = $_POST["branch"];
+    $date = $_POST["date"];
+    $time = $_POST["time"];
+    $user = $_POST["user"];
+    $invoice = $_POST["invoice"];
+    $po_number = $_POST["po_number"];
+    $table_items = json_decode($_POST["table_items"], true);
+
+    $mysql = "INSERT INTO tbl_store (supplier_name, branch, date, time, user, 
+  invoice_no, lpo_number) VALUES ('" . $supplier_name . "', 
+  '" . $branch . "', '" . $date . "', '" . $time . "', '" . $user . "', " . $invoice . ", " . $po_number . ");";
+    $mysql .= "SELECT receipt_no FROM tbl_store ORDER BY receipt_no DESC LIMIT 1";
+
+    if (mysqli_multi_query($conn, $mysql)) {
+        do {
+            /* store first result set */
+            if ($result = mysqli_store_result($conn)) {
+                while ($row = mysqli_fetch_row($result)) {
+                    $po_number = $row[0];
+                }
+                mysqli_free_result($result);
+            }
+            /* print divider */
+            if (mysqli_more_results($conn)) {
+                // echo "-----------------\n";
+            }
+        } while (mysqli_next_result($conn));
+        foreach ($table_items as $key => $value) {
+
+            $mysql = "INSERT INTO tbl_store_item (lpo_number, product_code, product_name, product_unit, qty, branch, product_cost, total)
+      VALUES('" . $po_number . "','" . $value["p_code"] . "','" . $value["p_name"] . "','" . $value["p_units"] . "',
+      '" . $value["p_quantity"] . "', '" . $branch . "','" . $value["p_cost"] . "' ,'" . $value["p_total"] . "')";
+            mysqli_query($conn, $mysql);
+
+            $sql = "UPDATE tbl_requisition_items SET status = 'done' WHERE status = 'approved' and branch= '$branch' and product_name='" . $value["p_name"] . "'";
+            mysqli_query($conn, $sql);
+        }
+
+        $sql = "SELECT * from tbl_requisition WHERE status= 'approved' and branch='$branch'";
+        $result = mysqli_query($conn, $sql);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $req_no = $row['requisition_No'];
+
+            $sql2 = "SELECT count(*) from tbl_requisition_items WHERE requisition_No='$req_no' and status='done'";
+            $sql1 = "SELECT count(*) from tbl_requisition_items WHERE requisition_No='$req_no'";
+
+            $result1 = mysqli_query($conn, $sql1);
+            $result2 = mysqli_query($conn, $sql2);
+
+            $row1 = mysqli_fetch_assoc($result1);
+            $row2 = mysqli_fetch_assoc($result2);
+
+            $all = $row1['count(*)'];
+            $done = $row2['count(*)'];
+
+            $bal = $all - $done;
+
+            if ($bal < 1) {
+                $sql = "UPDATE tbl_requisition SET status = 'done' WHERE requisition_No = '$req_no'";
+                mysqli_query($conn, $sql);
+            }
+        }
+
+
+        $message = "Purchase Order number " . $po_number . " Created Successfully..";
+        echo json_encode($message);
+    } else {
+        // echo "Multiquery failed: " . $mysql;
+        echo "Multi query failed: (" . $conn->errno . ") " . $conn->error;
+    }
+}
+
+mysqli_close($conn);
--- includes/filter_receive_goods.php
+++ includes/filter_receive_goods.php
@@ -6,9 +6,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $start_date = $_POST["date1"];
     $end_date = $_POST["date2"];
     $status = "approved";
+    $branch = $_SESSION['branch'];
 
 
-    $query = "SELECT * FROM tbl_purchaseorder WHERE status='$status' and date>='$start_date' and date<= '$end_date'";
+    $query = "SELECT * FROM tbl_purchaseorder WHERE status='$status' and branch='$branch' and date>='$start_date' and date<= '$end_date'";
 
     $result = mysqli_query($conn, $query);
     $response = array();
