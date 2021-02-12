4af7eb01179de7a0c0fca1d2a642782c203c2470
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-12 10:46:06 +0300

    update po

---
+++ includes/update_po.php
@@ -0,0 +1,23 @@
+<?php
+include_once '../includes/dbconnect.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  
+  $req_no = $_POST["po_number"];
+
+  $stats = "pending";
+
+    $sql1 = "UPDATE tbl_purchaseorder_items SET status = 'approved' WHERE requisition_No = '" . $req_no . "' and status= '$stats'";
+    $sql = "UPDATE tbl_purchaseorder SET status = 'approved' WHERE requisition_No = '" . $req_no . "'";
+    mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql1);
+    $response['message'] = "Selected Purchase Order Approved..";
+  
+  echo json_encode($response);
+
+  mysqli_close($conn);
+} else {
+  $response['message'] = "Fields Required";
+  echo json_encode($response);
+}
