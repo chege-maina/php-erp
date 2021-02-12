55fe9e5087c87152dcfcdc61b32ca80a7f71c50a
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-12 11:53:00 +0300

    kkkk

--- includes/filter_receive_goods.php
+++ includes/filter_receive_goods.php
@@ -3,34 +3,34 @@ include_once '../includes/dbconnect.php';
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $start_date = $_POST["date1"];
-  $end_date = $_POST["date2"];
-  $status = "approved";
+    $start_date = $_POST["date1"];
+    $end_date = $_POST["date2"];
+    $status = "approved";
 
 
-  $query = "SELECT * FROM tbl_purchaseorder WHERE status='$status' and date>='$start_date' and date<= '$end_date'";
+    $query = "SELECT * FROM tbl_purchaseorder WHERE status='$status' and date>='$start_date' and date<= '$end_date'";
 
-  $result = mysqli_query($conn, $query);
-  $response = array();
+    $result = mysqli_query($conn, $query);
+    $response = array();
 
-  while ($row = mysqli_fetch_assoc($result)) {
-    array_push(
-      $response,
-      array(
-        'po_number'=>$row['po_number'],
-        'date'=>$row['date'],
-        'supplier'=>$row['supplier_name'],
-        'user'=>$row['user'],
-        'status'=>$row['status'])
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push(
+            $response,
+            array(
+                'po_number' => $row['po_number'],
+                'date' => $row['date'],
+                'supplier' => $row['supplier_name'],
+                'user' => $row['user'],
+                'status' => $row['status']
+            )
         );
-     }
+    }
 
 
-  echo json_encode($response);
+    echo json_encode($response);
 
-  mysqli_close($conn);
+    mysqli_close($conn);
 } else {
-  $message = "Fields have no data...";
-  echo json_encode($message);
+    $message = "Fields have no data...";
+    echo json_encode($message);
 }
-
