168ef87c8d5b776ee93f3bb48fa6e55f425c5728
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-15 15:12:49 +0300

    filter

---
+++ includes/filter_transfers.php
@@ -0,0 +1,36 @@
+<?php
+include_once '../includes/dbconnect.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $start_date = $_POST["date1"];
+    $end_date = $_POST["date2"];
+    $status = $_POST["status"];
+    $branch = $_SESSION['branch'];
+
+
+    $query = "SELECT * FROM tbl_transfer WHERE status='$status' and branch='$branch' and date>='$start_date' and date<= '$end_date'";
+
+    $result = mysqli_query($conn, $query);
+    $response = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push(
+            $response,
+            array(
+                'req_no' => $row['transfer_no'],
+                'date' => $row['date'],
+                'branch' => $row['branch'],
+                'user' => $row['user'],
+                'status' => $row['status']
+            )
+        );
+    }
+
+    echo json_encode($response);
+
+    mysqli_close($conn);
+} else {
+    $message = "Fields have no data...";
+    echo json_encode($message);
+}
