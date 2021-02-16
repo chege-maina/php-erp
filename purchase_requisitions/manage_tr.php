c925af4c2b5a2fe52e3f2e25408af6aa594d0bcf
RuthKerubo <ruthkeggy@gmail.com>
2021-02-16 11:15:25 +0300

    resolved json

--- purchase_requisitions/manage_tr.php
+++ purchase_requisitions/manage_tr.php
@@ -203,9 +203,9 @@ include '../includes/base_page/head.php';
                     const table_body = document.querySelector("#table_body");
 
                     window.addEventListener('DOMContentLoaded', (event) => {
-                        //  if (sessionStorage.length === 0) {
-                        //    location.href = "./manage_transfer.php";
-                        // }
+                        if (sessionStorage.length === 0) {
+                            location.href = "./manage_transfer.php";
+                        }
 
                         // Get passed requisition number
                         reqNo = sessionStorage.getItem('req_no');
@@ -445,6 +445,34 @@ include '../includes/base_page/head.php';
                                 console.error('Error:', error);
                             });
 
+
+                        const product_branch = document.querySelector("#product_branch");
+                        formData.append("product_branch", product_branch.value);
+                        formData.append("req_no", reqNo);
+
+                        fetch('../includes/transfer_balance.php', {
+                                method: 'POST',
+                                body: formData
+                            })
+                            .then(response => response.json())
+                            .then(result => {
+                                const alertVar =
+                                    `<div class="alert alert-success alert-dismissible fade show" role="alert">
+              <strong>Success!</strong> ${result['message']}
+              <button class="btn-close" type="button" data-dismiss="alert" aria-label="Close"></button>
+              </div>`;
+                                var divAlert = document.querySelector("#alert-div");
+                                divAlert.innerHTML = alertVar;
+                                divAlert.scrollIntoView();
+
+                                window.setTimeout(() => {
+                                    location.href = "manage_tr.php"
+                                }, 2500);
+
+                            })
+                            .catch(error => {
+                                console.error('Error:', error);
+                            });
                     }
 
 
@@ -487,40 +515,36 @@ include '../includes/base_page/head.php';
                             });
 
 
+                        const product_branch = document.querySelector("#product_branch");
+                        formData.append("product_branch", product_branch.value);
+                        formData.append("req_no", reqNo);
+
+                        fetch('../includes/transfer_balance.php', {
+                                method: 'POST',
+                                body: formData
+                            })
+                            .then(response => response.json())
+                            .then(result => {
+                                const alertVar =
+                                    `<div class="alert alert-success alert-dismissible fade show" role="alert">
+<strong>Success!</strong> ${result['message']}
+<button class="btn-close" type="button" data-dismiss="alert" aria-label="Close"></button>
+</div>`;
+                                var divAlert = document.querySelector("#alert-div");
+                                divAlert.innerHTML = alertVar;
+                                divAlert.scrollIntoView();
+
+                                window.setTimeout(() => {
+                                    location.href = "manage_tr.php"
+                                }, 2500);
+
+                            })
+                            .catch(error => {
+                                console.error('Error:', error);
+                            });
+
+
                     }
-
-                    const formData = new FormData();
-
-                    const product_branch = document.querySelector("#product_branch");
-                    formData.append("product_branch", product_branch.value);
-                    formData.append("req_no", reqNo);
-
-                    fetch('../includes/transfer_balance.php', {
-                            method: 'POST',
-                            body: formData
-                        })
-                        .then(response => response.json())
-                        .then(result => {
-                            const alertVar =
-                                `<div class="alert alert-success alert-dismissible fade show" role="alert">
-              <strong>Success!</strong> ${result['message']}
-              <button class="btn-close" type="button" data-dismiss="alert" aria-label="Close"></button>
-              </div>`;
-                            var divAlert = document.querySelector("#alert-div");
-                            divAlert.innerHTML = alertVar;
-                            divAlert.scrollIntoView();
-
-                            window.setTimeout(() => {
-                                location.href = "manage_tr.php"
-                            }, 2500);
-
-                        })
-                        .catch(error => {
-                            console.error('Error:', error);
-                        });
-
-
-
 
                     function actionRespond(value) {
                         value = value.split("-");
