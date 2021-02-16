8fc2051da18bb5a508b79c1e468bba8163924cf8
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-16 14:17:41 +0300

    kkk

---
+++ includes/load_transfer_approve.php
@@ -0,0 +1,31 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+session_start();
+
+$stat = "approved";
+
+$query = "SELECT * FROM tbl_transfer WHERE status='$stat'";
+
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+    array_push(
+        $response,
+        array(
+            'transfer_no' => $row['transfer_no'],
+            'branch' => $row['branch'],
+            'date' => $row['date'],
+            'branch_from' => $row['branch_from'],
+            'user' => $row['user']
+        )
+    );
+}
+
+echo json_encode($response);
+
+mysqli_close($conn);
