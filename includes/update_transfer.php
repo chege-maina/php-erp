33d6e9ea34d6405ff90b78fd5c230e579fe7d925
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-16 16:35:03 +0300

    chs

--- includes/update_transfer.php
+++ includes/update_transfer.php
@@ -4,12 +4,13 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $checker = $_POST["checker"];
+    $name = $_POST["name"];
     $req_no = $_POST["req_no"];
 
     $stats = "pending";
 
     if (strcmp($checker, 'item_rejected') == 0) {
-        $sql = "UPDATE tbl_transfer_items SET status = 'rejected' WHERE transfer_no = '" . $req_no . "'";
+        $sql = "UPDATE tbl_transfer_items SET status = 'rejected' WHERE transfer_no = '" . $req_no . "', product_name = '" . $name . "'";
         mysqli_query($conn, $sql);
         $response['message'] = "Selected Item Removed From Transfer..";
     } else if (strcmp($checker, 'req_rejected') == 0) {
