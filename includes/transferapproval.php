2281e7f4d1df66a657d0ba9d5ea43da51b88a913
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-16 16:13:08 +0300

    sdddd

---
+++ includes/transferapproval.php
@@ -0,0 +1,30 @@
+<?php
+include_once '../includes/dbconnect.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $req_no = 1; //$_POST["po_number"];
+
+    $query = "SELECT * FROM tbl_transfer_items WHERE transfer_no ='$req_no'";
+
+    $result = mysqli_query($conn, $query);
+    $response = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push(
+            $response,
+            array(
+                'code' => $row['product_code'],
+                'name' => $row['product_name'],
+                'unit' => $row['product_unit'],
+                'qty' => $row['product_quantity']
+            )
+        );
+    }
+
+    echo json_encode($response);
+} else {
+    $message = "Fields have no data...";
+    echo json_encode($message);
+}
+mysqli_close($conn);
