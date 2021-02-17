79e5b7b555471edc6a64709b0220ec22389fb83d
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-17 16:31:55 +0300

    Create receieve_approval.php

---
+++ includes/receieve_approval.php
@@ -0,0 +1,31 @@
+<?php
+include_once '../includes/dbconnect.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $req_no = $_POST["po_number"];
+    $stat = 'accepted';
+
+    $query = "SELECT * FROM tbl_transfer_items WHERE transfer_no ='$req_no' and status='$stat'";
+
+    $result = mysqli_query($conn, $query);
+    $response = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push(
+            $response,
+            array(
+                'code' => $row['product_code'],
+                'name' => $row['product_name'],
+                'unit' => $row['product_unit'],
+                'qty' => $row['product_quantity']
+            )
+        );
+    }
+
+    echo json_encode($response);
+} else {
+    $message = "Fields have no data...";
+    echo json_encode($message);
+}
+mysqli_close($conn);
