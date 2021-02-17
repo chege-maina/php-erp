34bb2d3b6e8fe6869bbd7b5f5e63a831c9da006b
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-17 13:16:27 +0300

    Update update_transfer_approved.php

--- includes/update_transfer_approved.php
+++ includes/update_transfer_approved.php
@@ -15,7 +15,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         mysqli_query($conn, $sql2);
         $response['message'] = "Selected Transfer Rejected..";
     } else {
-        $sql1 = "UPDATE tbl_transfer_items SET status = 'done' WHERE transfer_no = '" . $req_no . "'";
+        $sql1 = "UPDATE tbl_transfer_items SET status = 'done' WHERE transfer_no = '" . $req_no . "' and status='approved'";
         $sql = "UPDATE tbl_transfer SET status = 'done' WHERE transfer_no = '" . $req_no . "'";
         mysqli_query($conn, $sql);
         mysqli_query($conn, $sql1);
