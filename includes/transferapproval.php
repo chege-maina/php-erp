30c7eb3745cd6ffc42295b17f17f6b54fd06bc30
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-17 13:09:57 +0300

    Update transferapproval.php

--- includes/transferapproval.php
+++ includes/transferapproval.php
@@ -3,28 +3,29 @@ include_once '../includes/dbconnect.php';
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $req_no = $_POST["po_number"];
+    $req_no = $_POST["po_number"];
+    $stat = 'approved';
 
-  $query = "SELECT * FROM tbl_transfer_items WHERE transfer_no ='$req_no'";
+    $query = "SELECT * FROM tbl_transfer_items WHERE transfer_no ='$req_no' and status='$stat'";
 
-  $result = mysqli_query($conn, $query);
-  $response = array();
+    $result = mysqli_query($conn, $query);
+    $response = array();
 
-  while ($row = mysqli_fetch_assoc($result)) {
-    array_push(
-      $response,
-      array(
-        'code' => $row['product_code'],
-        'name' => $row['product_name'],
-        'unit' => $row['product_unit'],
-        'qty' => $row['product_quantity']
-      )
-    );
-  }
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push(
+            $response,
+            array(
+                'code' => $row['product_code'],
+                'name' => $row['product_name'],
+                'unit' => $row['product_unit'],
+                'qty' => $row['product_quantity']
+            )
+        );
+    }
 
-  echo json_encode($response);
+    echo json_encode($response);
 } else {
-  $message = "Fields have no data...";
-  echo json_encode($message);
+    $message = "Fields have no data...";
+    echo json_encode($message);
 }
 mysqli_close($conn);
