c8484083ab9335bf486c6188a785ef3edd5b44df
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-17 14:51:06 +0300

    Update receive_goods_manage_items.php

--- includes/receive_goods_manage_items.php
+++ includes/receive_goods_manage_items.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stats = 'pending';
     $stats2 = 'approved';
 
-    $query = "SELECT * FROM tbl_purchaseorder_items WHERE po_number ='$req_no'";
+    $query = "SELECT * FROM tbl_purchaseorder_items WHERE po_number ='$req_no' and status<>'done'";
 
 
     $result = mysqli_query($conn, $query);
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $poqty = $row['product_quantity'];
         $name = $row['product_name'];
 
-        $query1 = "SELECT sum(qty) FROM tbl_store_item WHERE lpo_number ='$req_no' and branch='$branch' and product_name='$name' and status<>'done'";
+        $query1 = "SELECT sum(qty) FROM tbl_store_item WHERE lpo_number ='$req_no' and branch='$branch' and product_name='$name'";
         $result1 = mysqli_query($conn, $query1);
         if ($row1 = mysqli_fetch_assoc($result1)) {
             $totalstore = $row1['sum(qty)'];
