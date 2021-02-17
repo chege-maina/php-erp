70a19b48d79cc84096582d1f3266a96a4d3e827c
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-17 14:23:21 +0300

    Create load_realsese_items.php

---
+++ includes/load_realsese_items.php
@@ -0,0 +1,37 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $req_no = $_POST["req_no"];
+
+    $stat = "approved";
+
+    $query = "SELECT * FROM tbl_transfer WHERE status='$stat' and transfer_no='$req_no'";
+
+
+    $result = mysqli_query($conn, $query);
+    $response = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push(
+            $response,
+            array(
+                'req_no' => $row['transfer_no'],
+                'branch' => $row['branch'],
+                'date' => $row['date'],
+                'user' => $row['user']
+            )
+        );
+    }
+
+    echo json_encode($response);
+
+    mysqli_close($conn);
+} else {
+    $message = "Fields have no data...";
+    echo json_encode($message);
+}
