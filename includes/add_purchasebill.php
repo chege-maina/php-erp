0b0791936cec8235e8ac3cfbbf7298d9cf61081a
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-18 16:20:43 +0300

    Update add_purchasebill.php

--- includes/add_purchasebill.php
+++ includes/add_purchasebill.php
@@ -43,44 +43,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         '" . $value["p_code"] . "','" . $value["p_name"] . "','" . $value["p_units"] . "', '" . $value["p_quantity"] . "',
         '" . $value["p_cost"] . "','" . $value["p_total"] . "','" . $user . "', '" . $rec_no . "')";
             mysqli_query($conn, $mysql);
-
-
-            if ($value["p_quantity_received"] == $value["p_quantity"]) {
-                $sql = "UPDATE tbl_purchaseorder_items SET status = 'done' WHERE po_number = '$po_number' and product_name='" . $value["p_name"] . "'";
-                mysqli_query($conn, $sql);
-            } else {
-                $sql = "UPDATE tbl_purchaseorder_items SET status = 'partial' WHERE po_number = '$po_number' and product_name='" . $value["p_name"] . "'";
-                mysqli_query($conn, $sql);
-                $sql = "UPDATE tbl_purchaseorder SET status = 'partial' WHERE po_number = '$po_number'";
-                mysqli_query($conn, $sql);
-            }
         }
-
-        $sql = "SELECT * from tbl_purchaseorder WHERE (status= 'approved' or status='partial') and branch='$branch'";
-        $result = mysqli_query($conn, $sql);
-        while ($row = mysqli_fetch_assoc($result)) {
-            $req_no = $row['po_number'];
-
-            $sql2 = "SELECT count(*) from tbl_purchaseorder_items WHERE po_number='$req_no' and status='done'";
-            $sql1 = "SELECT count(*) from tbl_purchaseorder_items WHERE po_number='$req_no'";
-
-            $result1 = mysqli_query($conn, $sql1);
-            $result2 = mysqli_query($conn, $sql2);
-
-            $row1 = mysqli_fetch_assoc($result1);
-            $row2 = mysqli_fetch_assoc($result2);
-
-            $all = $row1['count(*)'];
-            $done = $row2['count(*)'];
-
-            $bal = $all - $done;
-
-            if ($bal < 1) {
-                $sql = "UPDATE tbl_purchaseorder SET status = 'done' WHERE po_number = '$req_no'";
-                mysqli_query($conn, $sql);
-            }
-        }
-
+        $sql = "UPDATE tbl_store_items SET status = 'done' WHERE receipt_no = '" . $rec_no . "'";
+        $sql2 = "UPDATE tbl_store SET status = 'done' WHERE receipt_no = '" . $rec_no . "'";
+        mysqli_query($conn, $sql);
+        mysqli_query($conn, $sql2);
 
         $message = "Goods Receipt Note " . $po_number . " Created Successfully..";
         echo json_encode($message);
