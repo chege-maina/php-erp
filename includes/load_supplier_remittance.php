905e9716bfe74494d0bf87f25be68d38963e0941
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-19 11:16:54 +0300

    Create load_supplier_remittance.php

---
+++ includes/load_supplier_remittance.php
@@ -0,0 +1,22 @@
+<?php
+include_once '../includes/dbconnect.php';
+session_start();
+
+$stat = "pending";
+
+$query = "SELECT supplier_name FROM tbl_purchase_bill WHERE status='$stat' GROUP BY supplier_name ASC";
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+  array_push(
+    $response,
+    array(
+      'supplier_name' => $row['supplier_name']
+    )
+  );
+}
+echo json_encode($response);
+
+mysqli_close($conn);
