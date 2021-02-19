ae09bcefe6ee62bebc0a4f414e9bb080eed65359
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-19 13:15:20 +0300

    Create filter_load_remittance.php

---
+++ includes/filter_load_remittance.php
@@ -0,0 +1,38 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $lpo_no = $_POST["supplier"];
+    $date = $_POST["date"];
+
+    $stat = "pending";
+
+    $query = "SELECT * FROM tbl_purchase_bill WHERE status='$stat' and supplier_name='$lpo_no' and due_date<='$date' ORDER BY due_date ASC";
+
+    $result = mysqli_query($conn, $query);
+    $response = array();
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $due = $row['due_date'];
+        $invoice = $row['invoice_no'];
+        $total = $row['total'];
+
+        array_push(
+            $response,
+            array(
+                'due_date' => $due,
+                'invoice_no' => $invoice,
+                'amount' => $total
+            )
+        );
+    }
+    echo json_encode($response);
+    mysqli_close($conn);
+} else {
+    $message = "Fields have no data...";
+    echo json_encode($message);
+}
