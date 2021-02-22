f9e83da9644ec5a8501e05d54abc668da8d846d1
RuthKerubo <ruthkeggy@gmail.com>
2021-02-22 13:21:45 +0300

    new db

---
+++ database/tbl_bank.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2021 at 10:41 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `loans`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_bank`
+--
+
+CREATE TABLE `tbl_bank` (
+  `bank_name` varchar(50) NOT NULL,
+  `branch_name` varchar(15) NOT NULL,
+  `acc_no` varchar(254) NOT NULL,
+  `acc_name` varchar(50) NOT NULL,
+  `currency` varchar(100) NOT NULL,
+  `opening_bal` varchar(254) NOT NULL,
+  `clear_days` varchar(50) NOT NULL,
+  `od_limit` varchar(100) NOT NULL,
+  `id_interest` varchar(100) NOT NULL,
+  `over_limit` varchar(100) NOT NULL,
+  `late_charges` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_bank`
+--
+
+INSERT INTO `tbl_bank` (`bank_name`, `branch_name`, `acc_no`, `acc_name`, `currency`, `opening_bal`, `clear_days`, `od_limit`, `id_interest`, `over_limit`, `late_charges`) VALUES
+('EQUITY BANK', 'KARATINA', '255445666', 'JUMANJI2', 'KSHS', '200', '3', '400', '12', '3', '3'),
+('EQUITY BANK', 'KARATINA', '52656663456', 'JUMANJI', 'KSHS', '150000', '7', '1000000', '4', '1000000', '3');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_bank`
+--
+ALTER TABLE `tbl_bank`
+  ADD PRIMARY KEY (`bank_name`,`acc_no`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
