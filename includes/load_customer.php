46f5e663b68b4e0af2826f5c5cd8cc2f51dc3618
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-23 11:03:44 +0300

    load quotation items

---
+++ includes/load_customer.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+session_start();
+
+
+$query = "SELECT * FROM tbl_customer";
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+  array_push(
+    $response,
+    array(
+      'name' => $row['name'],
+      'terms' => $row['payment_terms']
+    )
+  );
+}
+
+echo json_encode($response);
+
+mysqli_close($conn);
---
+++ includes/load_items_quote.php
@@ -0,0 +1,29 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+session_start();
+
+
+$query = "SELECT * FROM tbl_product";
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+  array_push(
+    $response,
+    array(
+      'code' => $row['product_code'],
+      'name' => $row['product_name'],
+      'unit' => $row['product_unit'],
+      'price' => $row['dsp_price']
+    )
+  );
+}
+
+echo json_encode($response);
+
+mysqli_close($conn);
