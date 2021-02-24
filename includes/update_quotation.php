7827f5106a143634fdf768206fc4648fa4343646
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-02-24 16:44:04 +0300

    Update update_quotation.php

--- includes/update_quotation.php
+++ includes/update_quotation.php
@@ -7,6 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_POST["name"];
   $qty = $_POST["qty"];
   $price = $_POST["price"];
+  $tax1 = $_POST["tax"];
   $req_no = $_POST["req_no"];
 
   $stats = "pending";
@@ -16,9 +17,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     mysqli_query($conn, $sql);
     $response['message'] = "Selected Item Removed From Requisition..";
   } else if (strcmp($checker, 'item_qty') == 0) {
-    $sql = "UPDATE tbl_quotation_items SET qty = '" . $qty . "' WHERE product_name = '" . $name . "' and quote_no = '" . $req_no . "'";
+    $tax = $price * $qty * ($tax1 / 100);
+    $sql = "UPDATE tbl_quotation_items SET qty = '" . $qty . "', price='" . $price . "' , tax='" . $tax . "' WHERE product_name = '" . $name . "' and quote_no = '" . $req_no . "'";
     mysqli_query($conn, $sql);
-    $response['message'] = "Selected Item Quantity Changed..";
+    $response['message'] = "Selected Item Details Changed..";
   } else if (strcmp($checker, 'req_rejected') == 0) {
     $sql = "UPDATE tbl_quotation_items SET status = 'rejected' WHERE quote_no = '" . $req_no . "'";
     $sql2 = "UPDATE tbl_quotation SET status = 'rejected' WHERE quote_no = '" . $req_no . "'";
