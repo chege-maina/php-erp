14699466f202ee0df0805f7de30a880a927063d6
Timothy Karani <kartimothy@gmail.com>
2021-02-25 11:19:38 +0300

    Showing data as received

--- sales_quotation/manage_qr.php
+++ sales_quotation/manage_qr.php
@@ -275,6 +275,11 @@ include '../includes/base_page/head.php';
               code_td.appendChild(document.createTextNode(data["code"]));
               code_td.classList.add("align-middle");
 
+
+              let code_td = document.createElement("td");
+              code_td.appendChild(document.createTextNode(data["code"]));
+              code_td.classList.add("align-middle");
+
               let name_td = document.createElement("td");
               name_td.appendChild(document.createTextNode(data["name"]));
               name_td.classList.add("align-middle", "w-25");
