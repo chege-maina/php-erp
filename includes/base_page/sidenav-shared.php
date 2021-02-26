79cc59d9d98ab0d84c0b433a9b72b836b4b07237
RuthKerubo <ruthkeggy@gmail.com>
2021-02-26 12:33:52 +0300

    changed UI

--- includes/base_page/sidenav-shared.php
+++ includes/base_page/sidenav-shared.php
@@ -138,7 +138,9 @@ function genSideBar()
   </li>
   <li class="nav-item"><a class="nav-link" href="../sales_order/create_sales_order.php">From Scratch</a>
   </li>
-  <li class="nav-item"><a class="nav-link" href="../sales_order/director_sale.php">Director Sales</a>
+  <li class="nav-item"><a class="nav-link" href="../sales_order/create_from_quotations2.php">Manage Sales Order</a>
+  </li>
+  <li class="nav-item"><a class="nav-link" href="../sales_order/dir_create_sales_order.php">Director Sales</a>
   </li>
 </ul>
 </li>
