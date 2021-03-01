3998f873cb3369e9d5c67da8b2c77812daec5811
qonsolidated <77890388+qonsolidated@users.noreply.github.com>
2021-03-01 13:55:24 +0300

    Create load_saleorder_invoice.php

---
+++ includes/load_saleorder_invoice.php
@@ -0,0 +1,34 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+session_start();
+session_start();
+$branch = $_SESSION['branch'];
+
+$stat = "approved";
+
+$query = "SELECT * FROM tbl_sale WHERE status='$stat''";
+
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+    array_push(
+        $response,
+        array(
+            'req_no' => $row['quote_no'],
+            'customer' => $row['customer_name'],
+            'date' => $row['date'],
+            'branch' => $row['branch_location'],
+            'user' => $row['user'],
+            'status' => $row['status']
+        )
+    );
+}
+
+echo json_encode($response);
+
+mysqli_close($conn);
