2dddbd9351e046990fcf13d350ba4fae3dca7ab3
Timothy Karani <kartimothy@gmail.com>
2021-03-01 18:29:45 +0300

    Got rid of vuetify

--- components/vue-components/pos-datatable/src/main.js
+++ components/vue-components/pos-datatable/src/main.js
@@ -1,10 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import Vue from 'vue';
+import PosComponent from './components/PosComponent.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(PosComponent),
+}).$mount('#app');
