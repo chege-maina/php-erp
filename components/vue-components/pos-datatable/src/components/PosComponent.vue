0dabbeb8e9b9f3c0b4b2450d04932af2b75a8736
Timothy Karani <kartimothy@gmail.com>
2021-03-02 10:27:59 +0300

    editable fields are editable

--- components/vue-components/pos-datatable/src/components/PosComponent.vue
+++ components/vue-components/pos-datatable/src/components/PosComponent.vue
@@ -3,7 +3,12 @@
     <table class="table table-sm table-striped table-hover">
       <thead>
         <tr>
-          <th scope="col" v-for="(item, index) in header" :key="index">
+          <th
+            scope="col"
+            v-bind:class="{ 'col-sm-1': item.editable }"
+            v-for="(item, index) in header"
+            :key="index"
+          >
             {{ item.name }}
           </th>
         </tr>
@@ -11,7 +16,13 @@
       <tbody>
         <tr v-for="item in body" :key="item.index">
           <td v-for="(value, key) in item" :key="key">
-            <span v-if="header_object[key].editable"> I feel it </span>
+            <span v-if="header_object[key].editable">
+              <input
+                class="form-control form-control-sm"
+                type="number"
+                v-bind:value="value"
+              />
+            </span>
             <span v-else>{{ value }}</span>
           </td>
         </tr>
