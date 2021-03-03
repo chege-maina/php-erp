c56e62b09ae625d0fa4357d34fdd9527c48ef291
Chege Maina <mr.shei8@gmail.com>
2021-03-03 15:17:58 +0300

    Create load_recadv_num.php

---
+++ includes/load_recadv_num.php
@@ -0,0 +1,24 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+
+$query = "SELECT * FROM tbl_receiptadv WHERE status='pending'";
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+	array_push(
+		$response,
+		array(
+			'rem_num' => $row['rem_no'],
+			'supplier' => $row['customer_name'],
+			'date' => $row['date']
+		)
+	);
+}
+echo json_encode($response);
+
+mysqli_close($conn);
