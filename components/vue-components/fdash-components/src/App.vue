f9f45e9f3e1c40fe5c519a4dcfb34ccffe7a7976
Timothy Karani <kartimothy@gmail.com>
2021-03-04 23:55:11 +0300

    Added base card controls

---
+++ components/vue-components/fdash-components/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <CountUpOrangey />
+  </div>
+</template>
+
+<script>
+import CountUpOrangey from "./components/CountUpOrangey.vue";
+
+export default {
+  name: "App",
+  components: {
+    CountUpOrangey,
+  },
+};
+</script>
