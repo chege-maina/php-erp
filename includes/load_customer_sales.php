ea80615fcbbdc49f320200eccc9692b1af79e09a
Chege Maina <mr.shei8@gmail.com>
2021-03-05 16:00:24 +0300

    Update load_customer_sales.php

--- includes/load_customer_sales.php
+++ includes/load_customer_sales.php
@@ -17,7 +17,7 @@ while ($row = mysqli_fetch_assoc($result)) {
   $terms = $row['payment_terms'];
   $limit = $row['credit_limit'];
 
-  $query2 = "SELECT sum(amount_due) FROM tbl_receipt_advice_items WHERE customer_name='$cust_name' and (status='pending' or status='approved')";
+  $query2 = "SELECT sum(amount_due) FROM tbl_receiptadv_items WHERE customer_name='$cust_name' and (status='pending' or status='approved')";
   $result2 = mysqli_query($conn, $query2);
   if ($row2 = mysqli_fetch_assoc($result2)) {
     $deni = $row2['sum(amount_due)'];
@@ -26,7 +26,8 @@ while ($row = mysqli_fetch_assoc($result)) {
   if ($balance == 0 || $balance < 0) {
     $status = "Limit Reached";
   } else {
-    $query3 = "SELECT due_date FROM tbl_receipt_advice_items WHERE customer_name='$cust_name' and due_date<'$date_chk' and (status='pending' or status='approved')";
+
+    $query3 = "SELECT due_date FROM tbl_receiptadv_items WHERE customer_name='$cust_name' and due_date<'$date_chk' and (status='pending' or status='approved')";
     $result3 = mysqli_query($conn, $query3);
     if ($row3 = mysqli_fetch_assoc($result3)) {
       $status = "Invoice Due";
