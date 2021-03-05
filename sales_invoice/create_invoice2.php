996122ce85d1d7bc29e2fd86df13d2b2dbb7c21a
RuthKerubo <ruthkeggy@gmail.com>
2021-03-05 17:30:53 +0300

    added changes

--- sales_invoice/create_invoice2.php
+++ sales_invoice/create_invoice2.php
@@ -133,7 +133,7 @@ include '../includes/base_page/head.php';
                 Transport Cost
               </div>
               <div class="col col-auto">
-                <input type="number" class="form-control text-right" id="transport" onkeyup="addTransport();" min="0" disabled required>
+                <input type="number" class="form-control text-right" id="transport" onkeyup="addTransport();" min="0" disabled>
               </div>
             </div>
             <div class="row m-3">
