6fe22cb72a657bd94ea5b6ad42884247e06f6da8
Timothy Karani <kartimothy@gmail.com>
2021-03-09 13:48:17 +0300

    Base component added

---
+++ components/vue-components/fpos-itemcards/src/components/ItemCard.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <h1>My Vue Web Component</h1>
+    <div>{{ msg }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["msg"],
+  mounted() {
+    const falcon_js = document.createElement("script");
+    falcon_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/assets/js/theme.min.js"
+    );
+    const anchor_js = document.createElement("script");
+    anchor_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/anchorjs/anchor.min.js"
+    );
+    const popper = document.createElement("script");
+    popper.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/popper/popper.min.js"
+    );
+    const bootstrap = document.createElement("script");
+    bootstrap.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/bootstrap/bootstrap.min.js"
+    );
+    const is_js = document.createElement("script");
+    is_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/is/is.min.js"
+    );
+    const prism = document.createElement("script");
+    prism.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/prism/prism.js"
+    );
+    const fontawesome = document.createElement("script");
+    fontawesome.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/fontawesome/all.min.js"
+    );
+    const lodash = document.createElement("script");
+    lodash.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/lodash/lodash.min.js"
+    );
+    const polyfill = document.createElement("script");
+    polyfill.setAttribute(
+      "src",
+      "https://polyfill.io/v3/polyfill.min.js?features,window.scroll"
+    );
+    const list_js = document.createElement("script");
+    list_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/vendors/list.js/list.min.js"
+    );
+    const config_js = document.createElement("script");
+    config_js.setAttribute(
+      "src",
+      "https://qonsolidated-solutions.github.io/falcon-assets/assets/js/config.js"
+    );
+
+    this.$el.prepend(config_js);
+    this.$el.append(
+      anchor_js,
+      popper,
+      bootstrap,
+      is_js,
+      prism,
+      fontawesome,
+      lodash,
+      polyfill,
+      list_js,
+      falcon_js
+    );
+
+    this.i_total = this.body.length / this.per_page;
+    this.i_total = this.i_total < 1 ? 1 : this.i_total;
+
+    this.i_total =
+      this.i_total.toString().split(".").length > 1
+        ? Number(this.i_total.toString().split(".")[0]) + 1
+        : this.i_total;
+  },
+};
+</script>
+
+<style>
+@import "https://qonsolidated-solutions.github.io/falcon-assets/assets/css/theme.min.css";
+</style>
