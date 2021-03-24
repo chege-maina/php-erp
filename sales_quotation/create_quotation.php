3c43736b5212b850aa2c861ad27fe418434f3d49
Timothy Karani <kartimothy@gmail.com>
2021-03-24 12:24:57 +0300

    Sending entered price

--- sales_quotation/create_quotation.php
+++ sales_quotation/create_quotation.php
@@ -238,14 +238,17 @@ include '../includes/base_page/head.php';
             p_name: item.name,
             p_tax_pc: item.tax,
             p_units: item.unit,
-            p_atomic_unit: item.atomic_unit,
-            p_quantity: item.quantity,
             p_price: item.price,
             p_amount: item.total,
             p_tax: item.tax_amt,
+            p_conversion: item.quantity,
+            p_atomic_unit: item.atomic_unit,
+            p_atomic_price: item.atomic_price,
             p_selected_unit: item.current_unit,
             p_quantity_in_base_unit: item.current_unit === "unit" ?
               item.quantity : item.quantity / item.conversion,
+            p_entered_price: item.current_unit === "unit" ?
+              item.price : item.atomic_price;
           })
         })
 
