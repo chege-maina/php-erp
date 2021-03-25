7f0ba12a6b202b2d2346acea7b792fa9bcf2a1e3
Timothy Karani <kartimothy@gmail.com>
2021-03-25 15:09:34 +0300

    Using UUID instead of button

--- components/vue-components/fpos-itemcards/src/components/ItemCard.vue
+++ components/vue-components/fpos-itemcards/src/components/ItemCard.vue
@@ -111,7 +111,15 @@ export default {
   },
   methods: {
     itemClicked: function () {
-      console.log(this.title);
+      console.log(this.title, " : ", this.uuid());
+    },
+    uuid: function () {
+      return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+        (
+          c ^
+          (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+      );
     },
   },
 };
