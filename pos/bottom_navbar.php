1594d37d9b050f136ae2b13ccadc03455889ae6c
Timothy Karani <kartimothy@gmail.com>
2021-03-29 16:33:42 +0300

    Tweaking UIs

--- pos/bottom_navbar.php
+++ pos/bottom_navbar.php
@@ -1,10 +1,10 @@
-<nav class="navbar fixed-bottom navbar-light py-2">
-  <div class="row container-fluid d-flex justify-content-end">
-    <div class="col col-auto">
-      <div class="card p-2">
+<nav class="navbar fixed-bottom navbar-light py-2 mr-0">
+  <div class="row container-fluid d-flex justify-content-end mr-0 p-0">
+    <div class="col col-auto p-0 mr-0">
+      <div class="card p-2 mr-0">
         <div class="row">
           <div class="col">
-            <button class="btn btn-falcon-primary">Checkout</button>
+            <button class="btn btn-falcon-default">Save</button>
           </div>
           <div class="col">
             <button class="btn btn-falcon-primary">Checkout</button>
