5f54c2faea7f92b94044a995a225d09757320cc6
Timothy Karani <kartimothy@gmail.com>
2021-03-29 11:41:01 +0300

    Added totals card

--- pos/pos.php
+++ pos/pos.php
@@ -421,19 +421,83 @@ if (!isset($_SESSION['loggedin'])) {
           </div>
 
           <div class="col">
-            <div class="card">
-              <div class="card-body fs--1 p-4">
-                Content is to start here
-                <div id="pos_component">
+            <div class="row">
+              <div class="col">
+                <div class="card">
+                  <div class="card-body fs--1 p-4">
+                    Content is to start here
+                    <div id="pos_component">
+                    </div>
+                    <script>
+                      window.addEventListener('DOMContentLoaded', (event) => {
+                        const pos_component = document.createElement("pos-component");
+                        pos_component.setAttribute("json_header", JSON.stringify(headers));
+                        pos_component.setAttribute("json_items", JSON.stringify(items));
+                        document.querySelector("#pos_component").appendChild(pos_component);
+                      });
+                    </script>
+                  </div>
                 </div>
-                <script>
-                  window.addEventListener('DOMContentLoaded', (event) => {
-                    const pos_component = document.createElement("pos-component");
-                    pos_component.setAttribute("json_header", JSON.stringify(headers));
-                    pos_component.setAttribute("json_items", JSON.stringify(items));
-                    document.querySelector("#pos_component").appendChild(pos_component);
-                  });
-                </script>
+              </div>
+            </div>
+            <div class="row my-2">
+              <div class="col">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="d-flex justify-content-end">
+                        <div class="col col-md-6">
+                          <table class="table table-sm table-striped table-hover">
+                            <tbody>
+                              <tr>
+                                <td class="text-right align-middle">Sales Discount</td>
+                                <td class="col">
+                                  <input type="text" class="form-control form-control-sm col-md-1" name="sales_discount" id="sales_discount">
+                                </td>
+                              </tr>
+                              <tr>
+                                <td class="text-right align-middle">Total Discount</td>
+                                <td class="col">
+                                  <input type="text" class="form-control form-control-sm col-md-1" name="sales_discount" id="sales_discount">
+                                </td>
+                              </tr>
+                              <tr>
+                                <td class="text-right align-middle">Total Tax</td>
+                                <td class="col">
+                                  <input type="text" class="form-control form-control-sm col-md-1" name="sales_discount" id="sales_discount">
+                                </td>
+                              </tr>
+                              <tr>
+                                <td class="text-right align-middle">Shipping Cost</td>
+                                <td class="col">
+                                  <input type="text" class="form-control form-control-sm col-md-1" name="sales_discount" id="sales_discount">
+                                </td>
+                              </tr>
+                              <tr>
+                                <td class="text-right align-middle">Grand Total</td>
+                                <td class="col">
+                                  <input type="text" class="form-control form-control-sm col-md-1" name="sales_discount" id="sales_discount">
+                                </td>
+                              </tr>
+                              <tr>
+                                <td class="text-right align-middle">Previous</td>
+                                <td class="col">
+                                  <input type="text" class="form-control form-control-sm col-md-1" name="sales_discount" id="sales_discount">
+                                </td>
+                              </tr>
+                              <tr>
+                                <td class="text-right align-middle">Change</td>
+                                <td class="col">
+                                  <input type="text" class="form-control form-control-sm col-md-1" name="sales_discount" id="sales_discount">
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
