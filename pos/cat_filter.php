07535958503566975ce363d905345bd238eadcc5
Timothy Karani <kartimothy@gmail.com>
2021-03-29 14:23:07 +0300

    Tracking file

---
+++ pos/cat_filter.php
@@ -0,0 +1,31 @@
+<div class="card">
+  <div class="card-body">
+    <div class="row">
+      <div class="col">
+        <input list="ice-cream-flavors" class="form-select form-select-sm" id="ice-cream-choice" name="ice-cream-choice" placeholder="Categories" />
+        <datalist id="ice-cream-flavors">
+          <option value="Chocolate">
+          <option value="Coconut">
+          <option value="Mint">
+          <option value="Strawberry">
+          <option value="Vanilla">
+        </datalist>
+      </div>
+      <div class="col">
+        <input list="bice-cream-flavors" class="form-select form-select-sm" id="ice-cream-choice" name="ice-cream-choice" placeholder="Sub Categories" />
+        <datalist id="bice-cream-flavors">
+          <option value="Chocolate">
+          <option value="Coconut">
+          <option value="Mint">
+          <option value="Strawberry">
+          <option value="Vanilla">
+        </datalist>
+      </div>
+      <div class="col col-auto">
+        <button class="btn btn-falcon-primary btn-sm">
+          Apply
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
