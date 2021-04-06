696b5b1133e25aa0e20392bcb203f4dd371b91ab
Timothy Karani <kartimothy@gmail.com>
2021-04-06 16:09:33 +0300

    Can calculate based on input values

--- components/vue-components/pos-datatable/src/components/PosComponent.vue
+++ components/vue-components/pos-datatable/src/components/PosComponent.vue
@@ -40,6 +40,7 @@
                 <select
                   class="form-select form-select-sm"
                   v-model="table_data[item.key].current_unit"
+                  v-on:click="unitUpdated(item.key, key)"
                 >
                   <option v-bind:value="table_data[item.key].units">
                     {{ table_data[item.key].units }}
@@ -212,6 +213,15 @@ export default {
         replacement_table[key] = this.table_data[key];
       }
       this.table_data = replacement_table;
+    },
+    unitUpdated(index, col) {
+      const current_unit = this.table_data[index].current_unit;
+      const units = this.table_data[index].units;
+      const price =
+        current_unit == units
+          ? this.table_data[index].price
+          : this.table_data[index].bulk_price;
+      this.table_data[index].current_price = price;
     },
   },
 };
