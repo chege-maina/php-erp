686d5de3428c7e050f824ec08936e4962c02f8ad
Timothy Karani <kartimothy@gmail.com>
2021-04-09 09:52:56 +0300

    Added base code for sessions

--- pos/above_datatable.php
+++ pos/above_datatable.php
@@ -1,13 +1,25 @@
 <div class="card">
-  <div class="card-body">
+  <div class="card-body py-3">
     <div class="row">
-      <div class="col">
-        <div class="input-group">
-          <input type="text" name="bar_code" id="bar_code" class="form-control form-control-sm" placeholder="Barcode">
-          <button class="btn btn-falcon-default btn-sm">
-            <span class="fas fa-barcode"></span>
-          </button>
+      <div class="col px-4">
+        <div class="row m-0 p-0">
+          <b>
+            <small>
+              John Doe
+            </small>
+          </b>
         </div>
+        <div class="row m-0 p-0">
+          <small>
+            Logged in at: 0000 HRS, 2/2/2000
+          </small>
+        </div>
+      </div>
+      <div class="col col-auto d-flex align-items-center">
+        <button class="btn btn-falcon-default btn-sm">
+          <span class="fas fa-door-open"></span>
+          End Session
+        </button>
       </div>
     </div>
   </div>
