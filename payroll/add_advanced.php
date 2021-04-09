015bcd2d4c4cb6c722c001b8abada6da56f3f181
Chege Maina <mr.shei8@gmail.com>
2021-04-09 09:39:27 +0300

    Update add_advanced.php

--- payroll/add_advanced.php
+++ payroll/add_advanced.php
@@ -15,12 +15,14 @@ function sanitize_input($data)
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $year = sanitize_input($_POST["year"]);
+  $month = sanitize_input($_POST["month"]);
   $table_items = json_decode($_POST["table_items"], true);
 
   foreach ($table_items as $key => $value) {
 
-    $mysql = "INSERT INTO tbl_emp_benefit (fname, lname, amount, 
-  nat, job, date_issued, year, month) VALUES('" . $fname . "','" . $lname . "','" . $type . "','" . $nat . "','" . $job . "','" . $value["benefit"] . "')";
+    $mysql = "INSERT INTO tbl_advance (fname, lname, amount, 
+  nat, job, date_issued, year, month) VALUES('" . $value["fname"] . "','" . $value["lname"] . "','" . $value["amount"] . "','" . $value["nat"] . "','" . $value["job"] . "','" . $value["date_issued"] . "','" . $year . "','" . $month . "')";
     mysqli_query($conn, $mysql);
   }
 
