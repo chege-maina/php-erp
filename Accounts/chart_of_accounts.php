3a538ebefb2def92d68f081e108430c575455f6a
Timothy Karani <kartimothy@gmail.com>
2021-04-12 11:49:08 +0300

    Base chart okay

--- Accounts/chart_of_accounts.php
+++ Accounts/chart_of_accounts.php
@@ -34,20 +34,11 @@ include '../includes/base_page/head.php';
         <!-- =========================================================== -->
         <!-- body begins here -->
         <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <h5 class="mb-2">Chart of Accounts</h5>
         <div class="card">
-          <div class="card-header bg-light">
-            <h5 class="mb-0">Change My Title</h5>
-          </div>
           <div class="card-body fs--1 p-4">
             <!-- Content is to start here -->
-            <div class="mb-3">
-              <label class="form-label" for="exampleFormControlInput1">Email address</label>
-              <input class="form-control" id="exampleFormControlInput1" type="search" placeholder="name@example.com" />
-            </div>
-            <div class="mb-3">
-              <label class="form-label" for="exampleFormControlTextarea1">Example textarea</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
+            Chart Goes Here
             <!-- Content ends here -->
           </div>
           <!-- Additional cards can be added here -->
