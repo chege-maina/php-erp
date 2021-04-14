0ed3b4201250cbad981275b05d8cc77650c9ee0b
Chege Maina <mr.shei8@gmail.com>
2021-04-14 09:05:43 +0300

    Update loadleave.php

--- payroll/loadleave.php
+++ payroll/loadleave.php
@@ -6,12 +6,26 @@ include_once '../includes/dbconnect.php';
 session_start();
 
 
-$query = "SELECT * FROM tbl_emp_leave  WHERE status='approved'";
+$query = "SELECT fname, lname, nat, job FROM tbl_emp_leave  WHERE status='approved' GROUP BY job ASC";
 
 $result = mysqli_query($conn, $query);
 $response = array();
 
 while ($row = mysqli_fetch_assoc($result)) {
+  $emp_id = $row['job'];
+
+  $query2 = "SELECT empleave FROM tbl_emp_leave  WHERE job = $emp_id";
+
+  $result2 = mysqli_query($conn, $query2);
+  $response2 = array();
+  while ($row2 = mysqli_fetch_assoc($result2)) {
+    array_push(
+      $response2,
+      array(
+        'leave' => $row['empleave']
+      )
+    );
+  }
 
   array_push(
     $response,
@@ -20,7 +34,7 @@ while ($row = mysqli_fetch_assoc($result)) {
       'lname' => $row['lname'],
       'nat' => $row['nat'],
       'job' => $row['job'],
-      'leave' => $row['empleave']
+      'leave' => $response2
     )
   );
 }
