2de72f51541e7c5fca3d346be86c4735557dca85
Timothy Karani <kartimothy@gmail.com>
2021-04-14 15:06:42 +0300

    Removed unneeded code

--- Accounts/components/tree_data_loader.php
+++ Accounts/components/tree_data_loader.php
@@ -1,71 +1,4 @@
 <script>
-  const items_to_show = [{
-      "name": "Assets",
-      "code": 1000,
-    },
-    {
-      "name": "Receivables",
-      "code": 1100,
-      "children": [{
-        "name": "zeta",
-        "children": [{
-            "name": "alpha",
-            "value": 10,
-            "file": "txt"
-          },
-          {
-            "name": "beta",
-            "value": 10,
-            "file": "txt"
-          },
-          {
-            "name": "gamma",
-            "value": 10,
-            "file": "txt"
-          },
-          {
-            "name": "delta",
-            "value": 10,
-            "file": "txt"
-          },
-          {
-            "name": "epsilon",
-            "value": 10,
-            "file": "txt"
-          },
-
-        ]
-      }, ],
-    },
-    {
-      "name": "Inventory",
-      "code": 1200,
-      "children": [{
-          "name": "static",
-          "children": [{
-            "name": "eta",
-            "value": 10,
-            "file": "txt"
-          }]
-        },
-        {
-          "name": "theta",
-          "value": 10,
-          "file": "txt"
-        },
-        {
-          "name": "iota",
-          "value": 10,
-          "file": "txt"
-        }
-      ]
-    },
-
-  ];
-
-  window.sessionStorage.clear();
-  window.sessionStorage.setItem("items", JSON.stringify(items_to_show));
-
   const root = {
     assets: {
       code: 1000,
@@ -118,8 +51,6 @@
   const index = {};
 
   createChildren(root);
-
-
 
 
   const object_to_array = [];
