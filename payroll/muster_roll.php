4efae799dadc91c98c4f86446674455ee16dbceb
Chege Maina <mr.shei8@gmail.com>
2021-04-16 15:27:34 +0300

    Update muster_roll.php

--- payroll/muster_roll.php
+++ payroll/muster_roll.php
@@ -2,52 +2,111 @@
 include_once '../includes/dbconnect.php';
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $paye = $_POST["paye"];
-  $nhif = $_POST["nhif"];
-  $year = $_POST["year"];
-  $month = $_POST["month"];
-  $stats = "pending";
-  $total_benef = 0;
-  $total_deduct = 0;
-  $query2 = "SELECT count(emp_no) FROM tbl_muster WHERE must_year ='$year' and must_month='$month'";
-  $result2 = mysqli_query($conn, $query2);
-  $row2 = mysqli_fetch_assoc($result2);
-  $counter = $row2['count(emp_no)'];
-  if ($counter == 0) {
+//if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$paye = "2012"; //$_POST["paye"];
+$nhif = "2012"; //$_POST["nhif"];
+$year = "2006"; //$_POST["year"];
+$month = "November"; //$_POST["month"];
+$stats = "pending";
+$total_benef = 0;
+$total_deduct = 0;
+$response = array();
+$query2 = "SELECT count(emp_no) FROM tbl_muster WHERE must_year ='$year' and must_month='$month'";
+$result2 = mysqli_query($conn, $query2);
+$row2 = mysqli_fetch_assoc($result2);
+$counter = $row2['count(emp_no)'];
+if ($counter == 0) {
 
 
-    //get employee number
-    $query = "SELECT emp_no FROM tbl_bene_deduct WHERE b_month ='$month' and b_year='$year' and status='$stats'";
-    $result = mysqli_query($conn, $query);
+  //get employee number
+  $query = "SELECT emp_no FROM tbl_bene_deduct WHERE b_month ='$month' and b_year='$year' and status='$stats'";
+  $result = mysqli_query($conn, $query);
 
-    while ($row = mysqli_fetch_assoc($result)) {
+  while ($row = mysqli_fetch_assoc($result)) {
 
-      $emp_no = $row['emp_no'];
-      //get total deductions
-      $query2 = "SELECT count(total) FROM tbl_bene_deduct WHERE b_year ='$year' and b_month='$month'and emp_no='$emp_no' and type='deduction'";
+    $emp_no = $row['emp_no'];
+    //get total deductions
+    $query2 = "SELECT sum(total) FROM tbl_bene_deduct WHERE b_year ='$year' and b_month='$month'and emp_no='$emp_no' and type='deduction'";
+    $result2 = mysqli_query($conn, $query2);
+    if ($row2 = mysqli_fetch_assoc($result2)) {
+      $total_deduct = $row2['sum(total)'];
+    }
+    // get total benefits 
+    $query2 = "SELECT sum(total) FROM tbl_bene_deduct WHERE b_year ='$year' and b_month='$month'and emp_no='$emp_no' and type='benefit'";
+    $result2 = mysqli_query($conn, $query2);
+    if ($row2 = mysqli_fetch_assoc($result2)) {
+      $total_benef = $row2['sum(total)'];
+    }
+
+    // get employee statics 
+    $query2 = "SELECT * FROM tbl_staff WHERE job_no='$emp_no' and status='approved'";
+    $result2 = mysqli_query($conn, $query2);
+    if ($row2 = mysqli_fetch_assoc($result2)) {
+      $salary = $row2['salary'];
+      $branch = $row2['branch'];
+      $emp_name = $row2['l_name'] . " " . $row2['f_name'] . " " . $row2['m_name'];
+      $tax_type = $row2['income_tax'];
+      $dept = $row2['department'];
+
+      //calculate PAYE
+      if (strcmp($tax_type, 'none') == 0) {
+        $paye_topay = 0;
+        $nhif_amt = 0;
+        $nssf_amt = 0;
+      } else {
+        $query2 = "SELECT * FROM tbl_paye WHERE descnhif='$paye' GROUP BY fromnhif ASC";
+        $result2 = mysqli_query($conn, $query2);
+        $paye_amt = 0;
+        $the_figure = $salary;
+        while ($row2 = mysqli_fetch_assoc($result2)) {
+          $upto = $row2['tonhif'];
+          $rate = $row2['rate'];
+          $relief = $row2['relief'];
+          if ($the_figure > $upto) {
+            $paye_amt = $paye_amt + ($upto * $rate / 100);
+          } else {
+            $paye_amt = $paye_amt + ($the_figure * $rate / 100);
+          }
+        }
+        $paye_topay = $paye_amt - $relief;
+        //get NHIF
+        $query2 = "SELECT rate FROM tbl_nhif WHERE fromnhif>='$salary' and tonhif<='$salary' and descnhif='$nhif'";
+        $result2 = mysqli_query($conn, $query2);
+        if ($row2 = mysqli_fetch_assoc($result2)) {
+          $nhif_amt = $row2['rate'];
+        }
+        //get NSSF
+        $query2 = "SELECT rate FROM tbl_nssf";
+        $result2 = mysqli_query($conn, $query2);
+        if ($row2 = mysqli_fetch_assoc($result2)) {
+          $nssf_amt = $row2['rate'];
+        }
+      }
+      //get Salary Advanced
+      $query2 = "SELECT amount FROM tbl_advance WHERE job='$emp_no' and year='$year' and month='$month'";
       $result2 = mysqli_query($conn, $query2);
       if ($row2 = mysqli_fetch_assoc($result2)) {
-        $total_deduct = $row2['count(total)'];
+        $advanced = $row2['amount'];
       }
-      // get total benefits 
-      $query2 = "SELECT count(total) FROM tbl_bene_deduct WHERE b_year ='$year' and b_month='$month'and emp_no='$emp_no' and type='benefit'";
-      $result2 = mysqli_query($conn, $query2);
-      if ($row2 = mysqli_fetch_assoc($result2)) {
-        $total_benef = $row2['count(total)'];
-      }
-
-      // get emplyee statics 
-      $query2 = "SELECT * FROM tbl_staff WHERE job_no='$emp_no' and status='approved'";
-      $result2 = mysqli_query($conn, $query2);
-      if ($row2 = mysqli_fetch_assoc($result2)) {
-        $salary = $row2['salary'];
-        $branch = $row2['branch'];
-        $emp_name = $row2['l_name'] . " " . $row2['f_name'] . " " . $row2['m_name'];
-        $tax_type = $row2['income_tax'];
-        $dept = $row2['department'];
-      }
-
+    }
+    array_push(
+      $response,
+      array(
+        'emp_no' => $emp_no,
+        'emp_name' => $emp_name,
+        'branch' => $branch,
+        'dept' => $dept,
+        'salary' => $salary,
+        'earnings' => $total_benef,
+        'paye' => $paye_topay,
+        'nssf' => $nssf_amt,
+        'nhif' => $nhif_amt,
+        'advanced' => $advanced,
+        'deductions' => $total_deduct,
+        'employer_nssf' => $nssf_amt
+      )
+    );
+    /*
       $amount_dis = $row['amount'];
       $interest = $row['interest'];
       $period = $row['period'];
@@ -99,9 +158,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
           $sql = "UPDATE tbl_loan_schedule SET status = 'paid' WHERE pay_no = '$pay_num ' and bank ='$bank' and loan_acc='$dis_date'";
           mysqli_query($conn, $sql);
         }
-      }
-    }
+      }*/
   }
+}/*
   $query2 = "SELECT * FROM tbl_loan_schedule WHERE bank ='$bank' and loan_acc='$dis_date' and status='pending' ORDER BY pay_date ASC LIMIT 1";
   $query = "SELECT * FROM tbl_loan_schedule WHERE bank ='$bank' and loan_acc='$dis_date' and status='pending'";
 
@@ -151,10 +210,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       )
     );
   }
-
-  echo json_encode($response2);
-} else {
+*/
+echo json_encode($response);
+/*} else {
   $message = "Fields have no data...";
   echo json_encode($message);
-}
+}*/
 mysqli_close($conn);
