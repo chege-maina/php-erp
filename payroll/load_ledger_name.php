694ea7d0417508f57c5b28f3a9b7082893362ab9
Chege Maina <mr.shei8@gmail.com>
2021-04-21 10:21:36 +0300

    Create load_ledger_name.php

---
+++ payroll/load_ledger_name.php
@@ -0,0 +1,20 @@
+<?php
+include_once '../includes/dbconnect.php';
+session_start();
+
+$query = "SELECT ledger_name FROM tbl_ledger";
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+  array_push(
+    $response,
+    array(
+      'ledger_name' => $row['ledger_name'],
+    )
+  );
+}
+echo json_encode($response);
+
+mysqli_close($conn);
