9f176b3a76f3805c09132b7fbce1aeca2a1c3e47
Timothy Karani <kartimothy@gmail.com>
2021-04-21 17:09:19 +0300

    UI basics okay

---
+++ Accounts/add_ledger_ui.php
@@ -0,0 +1,78 @@
+<?php
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+  header('Location: ../index.php');
+  exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+<?php
+include '../includes/base_page/head.php';
+?>
+
+
+
+<body>
+  <!-- ===============================================-->
+  <!--    Main Content-->
+  <!-- ===============================================-->
+  <main class="main" id="top">
+    <div class="container" data-layout="container">
+      <!--nav starts here -->
+      <?php
+      include '../includes/base_page/nav.php';
+      ?>
+
+      <div class="content">
+        <?php
+        include '../navbar-shared.php';
+        ?>
+
+        <!-- =========================================================== -->
+        <!-- body begins here -->
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <h5 class="mb-2">Add Ledger</h5>
+        <div class="card">
+          <div class="card-body fs--1 p-4">
+            <!-- Content is to start here -->
+            <div class="mb-3">
+              <label class="form-label" for="exampleFormControlInput1">Ledger Name</label>
+              <input class="form-control" id="exampleFormControlInput1" type="text" />
+            </div>
+            <div class="mb-3">
+              <label class="form-label" for="exampleFormControlInput1">Group Name</label>
+              <input class="form-control" id="exampleFormControlInput1" type="text" />
+            </div>
+            <!-- Content ends here -->
+          </div>
+          <!-- Additional cards can be added here -->
+        </div>
+
+        <div class="card p-2 mt-1">
+          <div class="row">
+            <div class="col-auto">
+              <button class="btn btn-falcon-primary">Submit</button>
+            </div>
+          </div>
+        </div>
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <!-- body ends here -->
+        <!-- =========================================================== -->
+
+
+
+        <!-- =========================================================== -->
+        <!-- Footer Begin -->
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <?php
+        include '../includes/base_page/footer.php';
+        ?>
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <!-- Footer End -->
+        <!-- =========================================================== -->
+</body>
+
+</html>
