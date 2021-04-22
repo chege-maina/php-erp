903d24fbe211b6d6c164ae8f9b3100fb8dd4475b
Timothy Karani <kartimothy@gmail.com>
2021-04-22 11:25:18 +0300

    ledger listing ui ready

---
+++ Accounts/ledger_listing_ui.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+  header('Location: ../index.php');
+  exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+<?php
+include '../includes/base_page/head.php';
+?>
+
+
+
+<body>
+  <!-- ===============================================-->
+  <!--    Main Content-->
+  <!-- ===============================================-->
+  <main class="main" id="top">
+    <div class="container" data-layout="container">
+      <!--nav starts here -->
+      <?php
+      include '../includes/base_page/nav.php';
+      ?>
+
+      <div class="content">
+        <?php
+        include '../navbar-shared.php';
+        ?>
+
+        <!-- =========================================================== -->
+        <!-- body begins here -->
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <h5 class="mb-0">Ledger List</h5>
+        <div class="card">
+          <div class="card-header bg-light">
+          </div>
+          <div class="card-body fs--1 p-4">
+            <!-- Content is to start here -->
+            <div class="mb-3">
+              <label class="form-label" for="exampleFormControlInput1">Email address</label>
+              <input class="form-control" id="exampleFormControlInput1" type="search" placeholder="name@example.com" />
+            </div>
+            <div class="mb-3">
+              <label class="form-label" for="exampleFormControlTextarea1">Example textarea</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <!-- Content ends here -->
+          </div>
+          <!-- Additional cards can be added here -->
+        </div>
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <!-- body ends here -->
+        <!-- =========================================================== -->
+
+
+
+        <!-- =========================================================== -->
+        <!-- Footer Begin -->
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <?php
+        include '../includes/base_page/footer.php';
+        ?>
+        <!-- -_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_- -->
+        <!-- Footer End -->
+        <!-- =========================================================== -->
+</body>
+
+</html>
