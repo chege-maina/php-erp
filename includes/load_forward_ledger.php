f439448289d51842a41fee7ae1d684e17841e11a
Chege Maina <mr.shei8@gmail.com>
2021-04-27 10:09:16 +0300

    combo shaet

---
+++ includes/load_forward_ledger.php
@@ -0,0 +1,26 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+session_start();
+
+
+$query = "SELECT * FROM tbl_ledger WHERE group_code='010301'";
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+    array_push(
+        $response,
+        array(
+            'ledger' => $row['ledger_name']
+        )
+    );
+}
+
+echo json_encode($response);
+
+mysqli_close($conn);
