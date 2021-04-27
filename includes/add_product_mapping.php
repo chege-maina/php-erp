7939664c60fe58ba5b7c14d3f9a0c8154555c8d4
Chege Maina <mr.shei8@gmail.com>
2021-04-27 12:40:41 +0300

    Create add_product_mapping.php

---
+++ includes/add_product_mapping.php
@@ -0,0 +1,39 @@
+<?php
+include_once '../includes/dbconnect.php';
+$con = $conn;
+if (mysqli_connect_errno()) {
+  // If there is an error with the connection, stop the script and display the error.
+  die('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+function sanitize_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $code = sanitize_input($_POST["code"]);
+  $name = sanitize_input($_POST["name"]);
+  $table_items = json_decode($_POST["table_items"], true);
+
+  foreach ($table_items as $key => $value) {
+
+    $mysql = "INSERT INTO tbl_prdmapping (product_code, product_name, 
+  group_code, ledger, status) VALUES('" . $code . "','" . $name . "','" . $value["group_code"] . "',
+  '" . $value["ledger"] . "','" . $value["status"] . "')";
+    mysqli_query($conn, $mysql);
+  }
+  if (strcmp($checker, 'from quote') == 0) {
+    $sql1 = "UPDATE tbl_quotation_items SET status = 'done' WHERE quote_no = '" . $quotation_no . "'";
+    $sql = "UPDATE tbl_quotation SET status = 'done' WHERE quote_no = '" . $quotation_no . "'";
+    mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql1);
+  }
+  $message = "Sales Order " . $quote_no . " Created Successfully..";
+  echo json_encode($message);
+} else {
+  echo "Multi query failed: (" . $conn->errno . ") " . $conn->error . "sql: " . $mysql;
+}
