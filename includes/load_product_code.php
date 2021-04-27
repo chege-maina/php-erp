1977a4d416df6a3a4684b4ae97405dffc86d9889
Chege Maina <mr.shei8@gmail.com>
2021-04-27 09:47:24 +0300

    Create load_product_code.php

---
+++ includes/load_product_code.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Content-type:application/json");
+
+include_once 'dbconnect.php';
+session_start();
+
+
+$query = "SELECT * FROM tbl_product";
+
+$result = mysqli_query($conn, $query);
+$response = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+    array_push(
+        $response,
+        array(
+            'name' => $row['product_name'],
+            'code' => $row['product_code']
+        )
+    );
+}
+
+echo json_encode($response);
+
+mysqli_close($conn);
