fe8e91401541b58992009447e0dec183ce50a7dd
Chege Maina <mr.shei8@gmail.com>
2021-05-05 10:05:22 +0300

    Create update-quotation.php

---
+++ includes/update-quotation.php
@@ -0,0 +1,45 @@
+<?php
+include_once '../includes/dbconnect.php';
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $checker = $_POST["checker"];
+  $name = $_POST["name"];
+  $qty = $_POST["qty"];
+  $price = $_POST["price"];
+  $tax1 = $_POST["tax"];
+  $req_no = $_POST["req_no"];
+
+  $stats = "pending";
+
+  if (strcmp($checker, 'item_rejected') == 0) {
+    $sql = "UPDATE tbl_quotation_items SET status = 'rejected' WHERE product_name = '" . $name . "' and quote_no = '" . $req_no . "'";
+    mysqli_query($conn, $sql);
+    $response['message'] = "Selected Item Removed From Quotation..";
+  } else if (strcmp($checker, 'item_qty') == 0) {
+    $tax = $price * $qty * ($tax1 / 100);
+    $total_amt = $tax + ($price * $qty);
+    $sql = "UPDATE tbl_quotation_items SET amount='$total_amt',qty = '" . $qty . "', price='" . $price . "' , tax='" . $tax . "' WHERE product_name = '" . $name . "' and quote_no = '" . $req_no . "'";
+    mysqli_query($conn, $sql);
+    $response['message'] = "Selected Item Details Changed..";
+  } else if (strcmp($checker, 'req_rejected') == 0) {
+    $sql = "UPDATE tbl_quotation_items SET status = 'rejected' WHERE quote_no = '" . $req_no . "'";
+    $sql2 = "UPDATE tbl_quotation SET status = 'rejected' WHERE quote_no = '" . $req_no . "'";
+    mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql2);
+    $response['message'] = "Selected Quotation Rejected..";
+  } else {
+    $sql1 = "UPDATE tbl_quotation_items SET status = 'approved' WHERE quote_no = '" . $req_no . "' and status= '$stats'";
+    $sql = "UPDATE tbl_quotation SET status = 'approved' WHERE quote_no = '" . $req_no . "'";
+    mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql1);
+    $response['message'] = "Selected Quotation Approved..";
+  }
+
+  echo json_encode($response);
+
+  mysqli_close($conn);
+} else {
+  $response['message'] = "Fields Required";
+  echo json_encode($response);
+}
